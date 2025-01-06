1ef31a071f6333fbccaf53ac77ad7ad33f442b28
Sarthak-0106 <vsarthak1234@gmail.com>
2025-01-06 14:58:27 +0530

    parsing the errors

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,3 @@ fn main() {
         Err(e) => eprintln!("❌ Error reading log file: {}", e),
     }
 }
-git config --global user.name "{your_username}"
-git config --global user.email "{your_email}"
-git remote set-url origin https://{your_username}:{your_token}@github.com/{your_username}/{your_repo}.gitgit config --global user.name "{your_username}"
